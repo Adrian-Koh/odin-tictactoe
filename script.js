37403bd59bc9a3e8f1a4f432d3e30871d88a9ea6
Adrian-Koh <adriankoh216@yahoo.com>
2025-06-14 15:50:21 +0800

    Add factories and modules for game flow

---
+++ script.js
@@ -0,0 +1,37 @@
+function createPlayer(name) {
+    return {name};
+}
+
+function createBoard() {
+    let grid = [['-', '-', '-'], ['-', '-', '-'], ['-', '-', '-']];
+    return {grid};
+}
+
+const gameController = function(board) {
+    const moveCount = 0;
+    function promptMove(isPlayer1Turn) {
+        const playerTurn = isPlayer1Turn ? 'Player 1' : 'Player 2';
+        let row = parseInt(prompt(playerTurn + ' enter row: 0-2'));
+        let col = parseint(prompt(playerTurn + ' enter column: 0-2'));
+
+        registerMove(row, col);
+    }
+    function nextMove() {
+        promptMove(moveCount % 2 === 0);
+        moveCount++;
+    }
+    function registerMove(row, col) {
+        if (board.grid[row][col] === '-') {
+            if (moveCount % 2 === 0) // player 1 turn (O)
+                board.grid[row][col] = 'O';
+            else                     // player 2 turn (X)
+                board.grid[row][col] = 'X';
+        }
+        else {
+            alert('This square has already been chosen!');
+        }
+    }
+
+    return {nextMove};
+}
+
