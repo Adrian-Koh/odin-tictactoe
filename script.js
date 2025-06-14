c8f9e269b80de28f4dd008e923e27a95ee843dad
Adrian-Koh <adriankoh216@yahoo.com>
2025-06-14 17:08:32 +0800

    Make game detect and announce winner

--- script.js
+++ script.js
@@ -9,17 +9,22 @@ function createBoard() {
 
 const gameController = function(board) {
     let moveCount = 0;
-    function promptMove(isPlayer1Turn) {
-        const playerTurn = isPlayer1Turn ? 'Player 1' : 'Player 2';
+    let hasWon = false;
+
+    function promptMove() {
+        const playerTurn = moveCount % 2 === 0 ? 'Player 1' : 'Player 2';
         let row = parseInt(prompt(playerTurn + ' enter row: 0-2'));
         let col = parseInt(prompt(playerTurn + ' enter column: 0-2'));
 
         registerMove(row, col);
     }
+
     function nextMove() {
-        promptMove(moveCount % 2 === 0);
+        promptMove();
+        checkWinner();
         moveCount++;
     }
+
     function registerMove(row, col) {
         if (board.grid[row][col] === '-') {
             if (moveCount % 2 === 0) // player 1 turn (O)
@@ -29,15 +34,38 @@ const gameController = function(board) {
         }
         else {
             alert('This square has already been chosen!');
+            promptMove();
         }
     }
+    function checkWinner() {
+        if (((board.grid[0][0] === board.grid[0][1]) && (board.grid[0][1] === board.grid[0][2]) && (board.grid[0][0] !== '-')) ||
+            ((board.grid[1][0] === board.grid[1][1]) && (board.grid[1][1] === board.grid[1][2]) && (board.grid[1][0] !== '-')) ||
+            ((board.grid[2][0] === board.grid[2][1]) && (board.grid[2][1] === board.grid[2][2]) && (board.grid[2][0] !== '-')) ||
+            ((board.grid[0][0] === board.grid[1][0]) && (board.grid[1][0] === board.grid[2][0]) && (board.grid[0][0] !== '-')) ||
+            ((board.grid[0][1] === board.grid[1][1]) && (board.grid[1][1] === board.grid[2][1]) && (board.grid[0][1] !== '-')) ||
+            ((board.grid[0][2] === board.grid[1][2]) && (board.grid[1][2] === board.grid[2][2]) && (board.grid[0][2] !== '-')) ||
+            ((board.grid[0][0] === board.grid[1][1]) && (board.grid[1][1] === board.grid[2][2]) && (board.grid[0][0] !== '-')) ||
+            ((board.grid[0][2] === board.grid[1][1]) && (board.grid[1][1] === board.grid[2][0]) && (board.grid[0][2] !== '-'))) {
+                if (moveCount % 2 === 0) { 
+                    alert('Player 1 has won!');
+                }
+                else {
+                    alert('Player 2 has won!');
+                }
+                hasWon = true;
+            }
+    }
 
-    return {nextMove};
+    function isGameOver() {
+        return hasWon;
+    }
+
+    return {nextMove, isGameOver};
 }
 
 let board = createBoard();
 const game = gameController(board);
-while (true) {
+while (!game.isGameOver()) {
     console.table(board.grid);
     game.nextMove();
 }
